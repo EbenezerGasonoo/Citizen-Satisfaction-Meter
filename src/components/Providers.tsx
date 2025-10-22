76755196e677e90a8779349ac2a5f4b99c73c0ae
Ebenezer Gasonoo <gas24001@byui.edu>
2025-10-22 20:12:53 +0000

    Fix SessionProvider SSR error: Create client-side Providers wrapper

---
+++ src/components/Providers.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from './ThemeProvider'
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  )
+}
